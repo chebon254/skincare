30a4bd46e02419d82d2936e0dbeaa05f91ba26bf
chebon254 <kelvinchebon90@gmail.com>
2022-09-23 22:34:06 +0300

    Code Changes

--- SkinCare/js/script.js
+++ SkinCare/js/script.js
@@ -97,3 +97,17 @@ function slowScroll() {
     location.href='#products';
   }, 1200);
 }
+
+/* == Cursor == */
+// (function (){
+//   var cursor = document.querySelector('.cursor');
+
+//   var editCursor = function editCursor(event){
+//      cursor.style.left = event.clientX + 'px';
+//      cursor.style.top = event.clientY + 'px';
+//   };
+
+//   window.addEventListener('mousemove', editCursor);
+
+// })();
+/* == || Cursor == */
