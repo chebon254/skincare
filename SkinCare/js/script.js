97802edb18793914a646f47075930cd810a96e77
chebon254 <kelvinchebon90@gmail.com>
2022-10-08 19:49:59 +0300

    Code changes

--- SkinCare/js/script.js
+++ SkinCare/js/script.js
@@ -150,3 +150,16 @@ function productClick() {
   location.href="product_detail.html";
 }
 /* == || ON PRODUCT CLICK == */
+/* == DROPDOWN SUMMARY == */
+$(document).ready(function(){
+  $(".default_option").click(function(){
+    $(this).parent().toggleClass("active");
+  })
+
+  $(".select_ul li").click(function(){
+    var currentele = $(this).html();
+    $(".default_option li").html(currentele);
+    $(this).parents(".select_wrap").removeClass("active");
+  })
+});
+/* == DROPDOWN SUMMARY == */
